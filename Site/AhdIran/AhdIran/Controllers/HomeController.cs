41caa9daf9e28881db255aec7e3a564b65babfa6
zavoshsoftware <70507208+zavoshsoftware@users.noreply.github.com>
2021-08-10 13:54:00 +0430

    text admin

--- Site/AhdIran/AhdIran/Controllers/HomeController.cs
+++ Site/AhdIran/AhdIran/Controllers/HomeController.cs
@@ -10,23 +10,54 @@ namespace AhdIran.Controllers
 {
     public class HomeController : Controller
     {
-       
+
         private DatabaseContext db = new DatabaseContext();
         public ActionResult Index()
         {
             HomeViewModel result = new HomeViewModel()
             {
-                Customers = db.Customers.Where(c => c.IsDeleted == false && c.IsActive).Take(8).ToList()
-              ,  Sliders = db.Sliders.Where(c => c.IsDeleted == false && c.IsActive).OrderBy(c=>c.Order).ToList()
+                Customers = db.Customers.Where(c => c.IsDeleted == false && c.IsActive).Take(8).ToList(),
+                Sliders = db.Sliders.Where(c => c.IsDeleted == false && c.IsActive).OrderBy(c => c.Order).ToList(),
+                Sec2Col1 = GetText("sec2-col1", "summery"),
+                Sec2Col2 = GetText("sec2-col2", "body"),
+                Sec2Col3 = GetText("sec2-col3", "body"),
+                ServiceText = GetText("home-service-text", "body"),
+                Features = db.TextItems.Where(c=>c.TextItemType.TypeName== "home-features" && c.IsDeleted==false).ToList(),
+                Process = db.TextItems.Where(c=>c.TextItemType.TypeName== "home-proccess" && c.IsDeleted==false).OrderBy(c=>c.Name).ToList(),
+                Values = db.TextItems.Where(c=>c.TextItemType.TypeName== "home-value" && c.IsDeleted==false).OrderBy(c=>c.Name).ToList(),
+                Address = GetText("address", "body"),
+                Phone = GetText("phone", "summery"),
+                Email = GetText("email", "summery"),
+                Instagram = GetText("insta", "summery"),
+                Telegram = GetText("telegram", "summery"),
             };
             return View(result);
+        }
+
+        public string GetText(string urlParam, string field)
+        {
+            TextItem textItem = db.TextItems.FirstOrDefault(c => c.Name == urlParam);
+
+            if (textItem != null)
+            {
+                if (field == "summery")
+                    return textItem.Summery;
+                if (field == "body")
+                    return textItem.Body;
+                if (field == "imageurl")
+                    return textItem.ImageUrl;
+                if (field == "title")
+                    return textItem.Title;
+            }
+
+            return String.Empty;
         }
         [Route("about")]
         public ActionResult About()
         {
             AboutViewModel result = new AboutViewModel()
             {
-                Customers = db.Customers.Where(c=>c.IsDeleted==false&&c.IsActive).Take(8).ToList()
+                Customers = db.Customers.Where(c => c.IsDeleted == false && c.IsActive).Take(8).ToList()
             };
             return View(result);
         }
--- Site/AhdIran/AhdIran/ViewModels/HomeViewModel.cs
+++ Site/AhdIran/AhdIran/ViewModels/HomeViewModel.cs
@@ -10,6 +10,18 @@ namespace ViewModels
     {
         public List<Customer> Customers { get; set; }
         public List<Slider> Sliders { get; set; }
+        public string Sec2Col1 { get; set; }
+        public string Sec2Col2 { get; set; }
+        public string Sec2Col3 { get; set; }
+        public List<TextItem> Features { get; set; }
+        public List<TextItem> Process { get; set; }
+        public List<TextItem> Values { get; set; }
+        public string ServiceText { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Instagram { get; set; }
+        public string Telegram { get; set; }
 
     }
 }
